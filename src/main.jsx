388d7597afa8490f4ef1aff438763ba31618dd27
winningeduofficial-jpg <winningeduofficial@gmail.com>
2026-05-29 04:17:53 +0900

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,26 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 
+function removePreHeader() {
+  const preHeader = document.getElementById('pre-header');
+
+  if (!preHeader) {
+    return;
+  }
+
+  preHeader.classList.add('is-hide');
+
+  window.setTimeout(() => {
+    preHeader.remove();
+  }, 260);
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+requestAnimationFrame(() => {
+  requestAnimationFrame(removePreHeader);
+});
