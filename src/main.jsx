be43bfad553138d2f2e8174d89d2ee7f6cdd2f0c
winningeduofficial-jpg <winningeduofficial@gmail.com>
2026-05-29 03:54:45 +0900

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -14,7 +14,27 @@ function removeFirstPaint() {
 
   window.setTimeout(() => {
     firstPaint.remove();
-  }, 220);
+  }, 620);
+}
+
+function waitForStablePaint() {
+  const fontReady = document.fonts ? document.fonts.ready : Promise.resolve();
+
+  const imageReady = new Promise((resolve) => {
+    const img = new Image();
+
+    img.onload = resolve;
+    img.onerror = resolve;
+    img.src = '/images/banner-1.png';
+  });
+
+  Promise.all([fontReady, imageReady]).then(() => {
+    requestAnimationFrame(() => {
+      requestAnimationFrame(() => {
+        window.setTimeout(removeFirstPaint, 180);
+      });
+    });
+  });
 }
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -23,6 +43,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   </React.StrictMode>
 );
 
-requestAnimationFrame(() => {
-  requestAnimationFrame(removeFirstPaint);
-});
+waitForStablePaint();
