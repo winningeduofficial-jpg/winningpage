af797199f8856d872351c3eef0e848030dcb9b12
winningeduofficial-jpg <winningeduofficial@gmail.com>
2026-05-28 22:50:27 +0900

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,1 +1,15 @@
+import HeroSlider from '../components/HeroSlider';
+import StatsBar from '../components/StatsBar';
+import ServiceCards from '../components/ServiceCards';
+import ReviewSection from '../components/ReviewSection';
 
+export default function Home() {
+  return (
+    <main className="bg-slate-50">
+      <HeroSlider />
+      <StatsBar />
+      <ServiceCards />
+      <ReviewSection />
+    </main>
+  );
+}
