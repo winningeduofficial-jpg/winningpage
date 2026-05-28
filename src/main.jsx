6249aa495157a40ff3610632559ccf6767d2837d
winningeduofficial-jpg <winningeduofficial@gmail.com>
2026-05-29 03:47:39 +0900

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,26 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 
+function removeFirstPaint() {
+  const firstPaint = document.getElementById('first-paint');
+
+  if (!firstPaint) {
+    return;
+  }
+
+  firstPaint.classList.add('is-hide');
+
+  window.setTimeout(() => {
+    firstPaint.remove();
+  }, 220);
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+requestAnimationFrame(() => {
+  requestAnimationFrame(removeFirstPaint);
+});
